187471083efa5ee4440a4edab60abd1c04fe375c
nawakarit <272197659+nawakarit-VOID@users.noreply.github.com>
2026-04-30 22:07:34 +0700

    ยัง error *1

---
+++ go.mod
@@ -0,0 +1,16 @@
+module test8
+
+go 1.26.2
+
+require (
+	gioui.org v0.9.0
+	golang.org/x/exp/shiny v0.0.0-20260410095643-746e56fc9e2f
+)
+
+require (
+	gioui.org/shader v1.0.8 // indirect
+	github.com/go-text/typesetting v0.3.0 // indirect
+	golang.org/x/image v0.39.0 // indirect
+	golang.org/x/sys v0.43.0 // indirect
+	golang.org/x/text v0.36.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,43 @@
+// Copyright (c) 2026 Nawakarit
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License v3.0.
+package main
+
+import (
+	"gioui.org/app"
+	"gioui.org/layout"
+	"gioui.org/op"
+	"gioui.org/widget"
+	"gioui.org/widget/material"
+)
+
+func main() {
+	go func() {
+		w := new(app.Window)
+		var ops op.Ops
+
+		th := material.NewTheme()
+		var btn widget.Clickable
+
+		for e := range w.Event() {
+			switch e := e.(type) {
+
+			case app.FrameEvent:
+				gtx := app.NewContext(&ops, e)
+
+				// ✅ ต้องใส่ gtx
+				for btn.Clicked(gtx) {
+					println("clicked!")
+				}
+
+				layout.Center.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+					return material.Button(th, &btn, "Click me").Layout(gtx)
+				})
+
+				e.Frame(gtx.Ops)
+			}
+		}
+	}()
+
+	app.Main()
+}
