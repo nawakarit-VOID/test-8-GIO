052775e8c86f370fe36bc2143b2a9a6ad349a153
nawakarit <272197659+nawakarit-VOID@users.noreply.github.com>
2026-05-01 19:47:02 +0700

    title, appsize, exit,

--- main.go
+++ main.go
@@ -4,44 +4,57 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"gioui.org/app"
 	"gioui.org/layout"
 	"gioui.org/op"
+	"gioui.org/unit"
 	"gioui.org/widget"
 	"gioui.org/widget/material"
 )
 
 func main() {
 	go func() {
+
 		w := new(app.Window)
-		var ops op.Ops
-
-		th := material.NewTheme()
-		var btn widget.Clickable
-
-		for {
-			e := w.Event() // ✅ ไม่ใช่ range แล้ว
-
-			switch e := e.(type) {
-
-			case app.DestroyEvent:
-				return // ตัวปิดหน้าต่าง
-
-			case app.FrameEvent:
-				gtx := app.NewContext(&ops, e)
-
-				for btn.Clicked(gtx) {
-					println("clicked!")
-				}
-
-				layout.Center.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
-					return material.Button(th, &btn, "Click me").Layout(gtx)
-				})
-
-				e.Frame(gtx.Ops)
-			}
+		w.Option(app.Title("คลิกๆ"), app.Size(unit.Dp(500), unit.Dp(500)))
+		//w := &app.Window{} //เหมือนกัน 100%  -- สร้างหน้าต่างเปล่า
+		//w.Option(app.Title("Custom Card Widgets ✨"), app.Size(unit.Dp(720), unit.Dp(440)))
+		//w := &app.Window{} //เหมือนกัน 100%
+		//app.Window = struct ของ window
+		//new(...) = สร้าง pointer ไปยัง struct นั้น
+		if err := run(w); err != nil {
+			log.Fatal(err)
 		}
+		os.Exit(0)
 	}()
+	app.Main() //เริ่มระบบของ Gio
+}
 
-	app.Main()
+func run(w *app.Window) error {
+	var ops op.Ops
+	th := material.NewTheme()
+	var btn widget.Clickable
+	for {
+		e := w.Event() // รับ event จาก OS
+		switch e := e.(type) {
+		case app.DestroyEvent:
+			return e.Err // ตัวปิดหน้าต่าง
+
+		case app.FrameEvent: //ใช้วาด UI
+			gtx := app.NewContext(&ops, e)
+
+			for btn.Clicked(gtx) {
+				println("clicked!")
+			}
+
+			layout.Center.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+				return material.Button(th, &btn, "Click me").Layout(gtx)
+			})
+
+			e.Frame(gtx.Ops)
+		}
+	}
 }
